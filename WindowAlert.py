9c7a15f0ee758d86deca5252a0db1c5aa3bc5bf8
Yakut <yakutozcan@gmail.com>
2016-11-23 20:17:30 +0200

    Create WindowAlert.py

---
+++ WindowAlert.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+from bs4 import BeautifulSoup
+import requests
+
+
+def bildirim(PencereDurumu):
+    renk = "green"
+    penceresimge = "window-new"
+    if PencereDurumu == "kapali":
+        renk="red"
+        penceresimge = "window-close"
+    mesaj = "\"<font size=8 color=" + renk + "><b><i>Pencere " + PencereDurumu + "</b></i></font>\""
+    komut = "notify-send DIKKAT " + mesaj + " -t 1000 -i "+penceresimge+""
+    os.system(komut)
+
+
+def Pencere_Kontrol():
+    r = requests.get("http://acikproje.com")
+    data = r.text
+    soup = BeautifulSoup(data, 'html.parser')
+    Sayfa = soup.find('p').getText()
+    bildirim(Sayfa)
+
+
+def main():
+    while 1:
+        Pencere_Kontrol()
+        #Sorgu suresi
+        time.sleep(5)
+    return 0
+
+if __name__ == '__main__':
+    main()
